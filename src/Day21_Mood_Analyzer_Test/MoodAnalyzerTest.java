876decdbf7205964ec369602e7bf469fe1d8a71e
bharaths1513 <bharaths.1311@gmail.com>
2021-11-16 22:47:30 +0530

    updated: repating test case 1.2

--- src/Day21_Mood_Analyzer_Test/MoodAnalyzerTest.java
+++ src/Day21_Mood_Analyzer_Test/MoodAnalyzerTest.java
@@ -8,10 +8,17 @@ import Day21_Mood_Analyzer.MoodAnalyzer;
 public class MoodAnalyzerTest {
 
 	@Test
-	public void testingRefactor() {
+	public void testingRefactor1() {
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
 		String mood = moodAnalyzer.analyzemood();
 		// Asserting The Result As Boolean Type True Or False
 		Assert.assertEquals("Sad", mood);
+	}
+
+	@Test
+	public void testingRefactor2() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+		String mood = moodAnalyzer.analyzemood();
+		Assert.assertEquals("Happy", mood);
 	}
 }
